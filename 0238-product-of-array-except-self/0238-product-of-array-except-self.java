a2524e69d4e5e0bfc801266d32bab6babd1acff7
Kiren Raj <107320570+iamkiren@users.noreply.github.com>
2023-03-03 14:10:47 +0530

    Time: 1 ms (100.00%), Space: 50.6 MB (52.01%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,25 +1,25 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int l = nums.length;
-        int[] left_Product  = new int[l];
-        int[] right_Product = new int[l];
+        
         int[] output_Array  = new int [l];
-
-        left_Product[0] = 1;
-        right_Product[l-1] = 1;
+       
+        output_Array[0] = 1;
 
         for(int i =1; i < l; i++){
-            left_Product[i] = left_Product[i-1] * nums[i-1];  
+            output_Array[i] = output_Array[i-1] * nums[i-1]; 
         }
 
-        for(int i = l-2; i >= 0 ; i--){
-            right_Product[i] = right_Product[i+1] * nums[i+1];  
-        }
-
-        for(int i = 0 ; i < l ; i++){
-            output_Array[i] = left_Product[i] * right_Product[i];
+    int rightProduct = 1;
+        for(int i = l-1; i >= 0 ; i--){
+            output_Array[i] = output_Array[i] * rightProduct;
+            rightProduct *= nums[i];
         }
 
         return output_Array;
     }
 }
+/*
+This is an optimised solution here => In the output array we are storing all left products first
+then in next step we use a variable to store all the right products and then multiplied into the current element in the output array see the video in the notes for better explanation
+*/
