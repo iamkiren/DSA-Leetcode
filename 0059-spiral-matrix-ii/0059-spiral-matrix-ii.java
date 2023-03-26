dde0390b28c60b3e07cbbf26d6b6187f7e92618a
Kiren Raj <107320570+iamkiren@users.noreply.github.com>
2023-03-26 19:36:32 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (20.80%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.java
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.java
@@ -1,72 +1,44 @@
 class Solution {
-    public int[][] generateMatrix(int n) 
-    {
-        int[][] arr=new int[n][n];
-		boolean a=true;
-		boolean b=false;
-		int i=0;
-		int j=0;
-		int q=0;
-		int p=-1;
-		int r=n;
-		int s=n;
-		int count=0;
-		while(i<n&&j<n&&count<n*n)
-		{
-			count++;
-			
-			if(a==true&&b==false)
-			{
-				arr[i][j]=count;
-				j++;
-				if(j==r)
-				{
-					j--;
-					i++;
-					a=false;
-					r--;
-				}
-			}
-			else if(a==false&&b==false)
-			{
-				arr[i][j]=count;
-				i++;
-				if(i==s)
-				{
-					j--;
-					i--;
-					b=true;
-					s--;
-				}
-			}
-			else if(b==true&&a==false)
-			{
-				arr[i][j]=count;
-				//System.out.print("(i:"+i+" j:"+j+") ");
-				j--;
-				if(j==p)
-				{
-					i--;
-					j++;
-					a=true;
-					p++;
-					
-				}
-			}
-			else if(b==true&&a==true)
-			{
-				arr[i][j]=count;
-				i--;
-				if(i==q)
-				{
-					i++;
-					j++;
-					b=false;
-					q++;
-				}
-				
-			}
-		}
-        return arr;
+    public int[][] generateMatrix(int n) {
+       int[][] ans=new int[n][n];
+       
+        int rowBegin=0;
+        int rowEnd=n-1;
+        int columnBegin=0;
+        int columnEnd=n-1;
+        int counter=0;
+      
+        while(rowBegin<=rowEnd && columnBegin<=columnEnd && counter<=n*n){
+            
+            for(int i=columnBegin;i<=columnEnd;i++){
+                counter++;
+                ans[rowBegin][i]=counter;
+            }
+            rowBegin++;
+            for(int i=rowBegin;i<=rowEnd;i++){
+                counter++;
+                ans[i][columnEnd]=counter;
+            }
+            columnEnd--;
+            if(rowBegin<=rowEnd){
+                for(int i=columnEnd;i>=columnBegin;i--){
+                counter++;
+                ans[rowEnd][i]=counter;
+            }
+           
+            }
+             rowEnd--;
+            if(columnBegin<=columnEnd){
+                for(int i=rowEnd;i>=rowBegin;i--){
+                counter++;    
+                ans[i][columnBegin]=counter;
+            }
+            
+            }
+            columnBegin++;
+           
+        }
+        
+        return ans;
     }
 }
