593a0b983eb04b5db39187af442dfdf7d874667d
Kiren Raj <107320570+iamkiren@users.noreply.github.com>
2023-04-02 21:02:24 +0530

    Time: 3 ms (96.15%) | Memory: 43.1 MB (89.64%) - LeetSync

--- spiral-matrix-iii/spiral-matrix-iii.java
+++ spiral-matrix-iii/spiral-matrix-iii.java
@@ -9,20 +9,16 @@ class Solution {
     }
     
     public int[][] spiralMatrixIII(int R, int C, int r0, int c0) {
-    int[][] dirt = new int[][] {{0, 1}, {1, 0}, {0, -1}, { -1, 0}}; // east, south, west, north is 0, 1, 2, 3
-    int[][] res = new int[R * C][2];
-    int len = 0, d = 0, j = 0; // move <len> steps in the <d> direction
-    res[j++] = new int[] {r0, c0};
-    while (j < R * C) { // fill all the blanks
-        if (d == 0 || d == 2) len++; // when move east or west, the length of path need plus 1
-        for (int i = 0; i < len; i++) {
-            r0 += dirt[d][0];
-            c0 += dirt[d][1];
-            if (r0 >= 0 && r0 < R && c0 >= 0 && c0 < C) // check valid
-                res[j++] = new int[] {r0, c0};
+        int r = r0, c = c0, len = 1;
+        ret = new int[R * C][2];
+        while (idx < (R * C )) {         
+            for (int k = 0; k < len; k++) add(r, c++, R, C);          
+            for (int k = 0; k < len; k++) add(r++, c, R, C);
+            len++;
+            for (int k = 0; k < len; k++) add(r, c--, R, C);
+            for (int k = 0; k < len; k++) add(r--, c, R, C);
+            len++;   
         }
-        d = (d + 1) % 4; // turn to next direction
+        return ret;
     }
-    return res;
-}
 }
