5a88347866e850b15f5d642d953e847b7725c76c
Kiren Raj <107320570+iamkiren@users.noreply.github.com>
2023-04-03 21:04:12 +0530

    Time: 1 ms (99.97%) | Memory: 43.3 MB (20.91%) - LeetSync

--- jump-game/jump-game.java
+++ jump-game/jump-game.java
@@ -2,12 +2,19 @@ class Solution {
     public boolean canJump(int[] nums) {
         int lastGoodIndexPosition = nums.length - 1;
 
-        for(int i = nums.length - 1; i >=0 ; i--){
-            if(i + nums[i] >= lastGoodIndexPosition){
+        for (int i = nums.length - 1; i >= 0 ; i--) {
+            if (i + nums[i] >= lastGoodIndexPosition) {
                 lastGoodIndexPosition = i;
             }
-        } 
+        }
 
-        return lastGoodIndexPosition == 0;     
+        return lastGoodIndexPosition == 0;
     }
 }
+/*
+Here the idea is going from backwards to the starting position
+We check from  each position weather can we go to lastGoodPosition (initially which is last position)
+and so on
+Finally we check weather we reached the initial position
+Explanation in the video linked to notes
+*/
